522f3a115f086b01328cff97cfc2bd6fb6d52198
poganv <33030522+poganv@users.noreply.github.com>
2018-01-27 17:03:40 +0200

    v

---
+++ js/gps1.js
@@ -0,0 +1,14 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.1";
+
+navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
+
+//#########################################
+function on_position_success(e)
+{
+	 document.getElementById("id_lat").innerHTML = "Latitude =" +e.coords.latitude;
+	 document.getElementById("id_long").innerHTML ="Longitude =" +e.coords.longitude;
+	 document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
+	 document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
+	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
+"&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0";

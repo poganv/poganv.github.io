d718fcd649140148150fcf3b4830edd814e1610e
poganv <33030522+poganv@users.noreply.github.com>
2018-01-27 16:27:06 +0200

    d

---
+++ js/gps.js
@@ -0,0 +1,29 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.08.4";
+
+navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
+
+//#########################################
+function on_position_success(e)
+{
+	document.getElementById("id_lat").innerHTML = "Latitude =" +e.coords.latitude;
+	document.getElementById("id_long").innerHTML ="Longitude =" +e.coords.longitude;
+	document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
+	document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
+	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
+"&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
+"&markers=color:blue|label:A|"+
+	  e.coords.latitude  + "," + e.coords.longitude + 
+	  "&path=color:red|" + e.coords.latitude + "," + e.coords.longitude + "|Sala Unirii Alba Iulia"
+	  ;
+document.getElementById("id_img").src=map_str;
+	
+}
+//#########################################
+function on_position_failure(e)
+{
+	
+	alert("I'm lost!");
+	
+}
+//cheia api google
